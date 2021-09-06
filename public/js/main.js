2f51e166884e21c6e86935e92e0cea5f6bd4d547
Rodrigo <rodrigocosta47@outlook.br>
2021-09-06 14:34:47 -0300

    Adicionei a funcionalida de contador de tempo

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 var wordCounterRecever = $('.word-couter'),
-letterCounterRecever = $('.letter-couter'),
-typer = $('#typer');
+    letterCounterRecever = $('.letter-couter'),
+    typer = $('#typer'),
+    seconds = $('.seconds'),
+    secondsCounter = seconds.text()
 
 typer.on('input', function () {
     var typervalue = typer.val();
@@ -12,4 +14,16 @@ typer.on('input', function () {
     var wordCount = typervalue.split(/\S+/).length - 1;
     wordCounterRecever.text(wordCount);
     console.log(wordCount);
+});
+
+typer.on('click', function(){
+
+    var conterInterval = setInterval(function(){
+        secondsCounter--;
+        seconds.text(secondsCounter);
+        if (secondsCounter < 1){
+            typer.attr('disabled', true);
+            clearInterval(conterInterval);
+        };
+    }, 1000);
 });
