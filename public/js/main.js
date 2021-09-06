4259bb0ad7e6f4c62d56610e0928b95e7dfe230a
Rodrigo <rodrigocosta47@outlook.br>
2021-09-06 16:44:36 -0300

    Refaturei o codigo em funcoes e adicionei a funcionalidade de reniciar o jogo

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,60 @@
-// I got the variables here
+// I got the main variables here
 var wordCounterRecever = $('.word-couter'),
     letterCounterRecever = $('.letter-couter'),
     typer = $('#typer'),
     seconds = $('.seconds'),
-    secondsCounter = seconds.text()
+    resetButton = $('#reset');
 
-// I created a word and caracteres counter
-typer.on('input', function () {
-    var typervalue = typer.val();
 
-    typervalueletter = typervalue.replace(/\s+/g, "");
-
-    letterCounterRecever.text(typervalueletter.length);
-
-    var wordCount = typervalue.split(/\S+/).length - 1;
-    wordCounterRecever.text(wordCount);
-    console.log(wordCount);
+$(document).ready(function () {
+    startsCounter();
+    caracterCounter();
+    resetGame();
 });
 
-// starts here the second counter
 
-typer.one('input', function(){
+function caracterCounter() {
+    // I created a word and caracteres counter
+    typer.on('input', function () {
+        var typervalue = typer.val();
 
-    var conterInterval = setInterval(function(){
-        secondsCounter--;
-        seconds.text(secondsCounter);
-        if (secondsCounter < 1){
-            typer.attr('disabled', true);
-            clearInterval(conterInterval);
-        };
-    }, 1000);
-});
+        typervalueletter = typervalue.replace(/\s+/g, "");
+        letterCounterRecever.text(typervalueletter.length);
+
+        var wordCount = typervalue.split(/\S+/).length - 1;
+        wordCounterRecever.text(wordCount);
+    });
+}
+
+
+
+function startsCounter() {
+    // starts here the second counter
+    var secondsCounter = seconds.text()
+    typer.one('input', function () {
+        var counterInterval = setInterval(function () {
+            secondsCounter--;
+            seconds.text(secondsCounter);
+            if (secondsCounter < 1) {
+                typer.attr('disabled', true);
+                clearInterval(counterInterval);
+            }
+            resetButton.on('click', function(){
+                clearInterval(counterInterval)
+                resetGame();
+            });
+        }, 1000);
+    });
+}
+
+function resetGame(){
+    var inicialSeconds = seconds.text()
+    resetButton.on('click', function () {
+        typer.val("")
+        seconds.text(inicialSeconds);
+        typer.attr('disabled', false);
+        wordCounterRecever.text('0');
+        letterCounterRecever.text('0');
+        startsCounter()
+    });
+};
