39ab2ea1a8d5da83f8d82962624ba53d0f7e7e17
Rodrigo <rodrigocosta47@outlook.br>
2021-09-06 17:31:03 -0300

    Configurando ignore e gulp

---
+++ public/gulpfile.js
@@ -0,0 +1,6 @@
+function defaultTask(gitcb) {
+    // place code for your default task here
+    cb();
+  }
+  
+  exports.default = defaultTask
--- public/js/main.js
+++ public/js/main.js
@@ -17,10 +17,8 @@ function caracterCounter() {
     // I created a word and caracteres counter
     typer.on('input', function () {
         var typervalue = typer.val();
-
         typervalueletter = typervalue.replace(/\s+/g, "");
         letterCounterRecever.text(typervalueletter.length);
-
         var wordCount = typervalue.split(/\S+/).length - 1;
         wordCounterRecever.text(wordCount);
     });
